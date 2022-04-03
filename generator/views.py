be8814a013ed88dbe60b384d0e56c8b34de69126
varsey <eugeny.varseev@gmail.com>
2022-04-04 00:19:05 +0300

    feat [generator/templates] generator 'home' template has new elements
    - generate button
    - pass length selector

--- generator/views.py
+++ generator/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'generator/home.html')
+    return render(request, 'generator/home.html', {'password': 'sdfsdfsf'})
