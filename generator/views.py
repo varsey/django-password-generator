f4994af0254da03128eaa8b9cbd722f91e931ec5
varsey <eugeny.varseev@gmail.com>
2022-04-04 22:32:30 +0300

    feat [generator] about page added, html templates rewritten

--- generator/views.py
+++ generator/views.py
@@ -5,6 +5,9 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'generator/home.html')
 
+def about(request):
+    return render(request, 'generator/about.html')
+
 def password(request):
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
--- password_generator/urls.py
+++ password_generator/urls.py
@@ -19,5 +19,5 @@ from generator import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('password/', views.password, name='password'),
-
+    path('about/', views.about, name='about'),
 ]
