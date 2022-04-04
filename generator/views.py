defc61deb59e4e413bf24591a3fece331c94cd0e
varsey <eugeny.varseev@gmail.com>
2022-04-04 22:21:44 +0300

    feat [generator/views] generates random string with parameters
    - numbers
    - special
    - uppercase

--- generator/views.py
+++ generator/views.py
@@ -6,9 +6,20 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
+
     characters = list('abcdefghijklmnopqrstuvwxyz')
-    length = 10
+
+    if request.GET.get('uppercase'):
+        characters.extend([x.upper() for x in characters])
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+
     thepassword = ''
     for _ in range(length):
         thepassword += random.choice(characters)
+
     return render(request, 'generator/password.html', {'password': thepassword})
